bf45bd10dc96be392b853c6df67675b4af9a9a63
MexiQQ <mexiQQ@users.noreply.github.com>
2015-02-25 11:55:49 +0800

    Update OAuthSignUtil.h

--- OAuthSignClass/OAuthSignUtil.h
+++ OAuthSignClass/OAuthSignUtil.h
@@ -7,24 +7,24 @@
 //
 
 //github 公司
-#define kGithubClientID @"b15cfa4c38d52ce679bd"
-#define kGithubClientSecret @"d4d3577156895553a2ebc5337038fe425886e074"
-#define kGithubRedirectUrl @"http://segmentfault.com/user/oauth/github"
+#define kGithubClientID @""
+#define kGithubClientSecret @""
+#define kGithubRedirectUrl @""
 
 //google 公司
-#define KGoogleClientID @"1036396677040.apps.googleusercontent.com"
-#define kGoogleClientSecret @"bEbv0ErmM-5VdajbNTR0SOYl"
-#define kGoogleRedirectUrl @"http://segmentfault.com/user/oauth/google"
+#define KGoogleClientID @""
+#define kGoogleClientSecret @""
+#define kGoogleRedirectUrl @""
 
 //QQ 公司
-#define KQQClientID @"100522525"
-#define kQQClientSecret @"847a2742c2fe5d6c13e5fbb68967f128"
-#define kQQRedirectUrl @"http://segmentfault.com/user/oauth/qq"
+#define KQQClientID @""
+#define kQQClientSecret @""
+#define kQQRedirectUrl @""
 
 //sina
-#define KSinaClientID @"1742025894"
-#define KSinaClientSecret @"c18d2bbaf9a6cb9344e994292ebab29f"
-#define KSinaRedirectUrl @"http://segmentfault.com/user/oauth/qq"
+#define KSinaClientID @""
+#define KSinaClientSecret @""
+#define KSinaRedirectUrl @""
 
 #import <Foundation/Foundation.h>
 #import "MXOAuthViewController.h"
